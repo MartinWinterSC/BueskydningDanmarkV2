ddb1a3f6590da17613285e1d0dca77eee9a0b422
BornefeltTheCoder <Nobornefelt@gmail.com>
2025-06-15 17:08:30 +0200

    Rettede prøvbueskydning
    Slttede duplikering

--- src/views/bredde/proevBueskydning.vue
+++ src/views/bredde/proevBueskydning.vue
@@ -85,95 +85,9 @@ const toggleAccordion = (index) => {
         </dl>
     </section>
     <section>
-        <h2>Alt der er værd at vide om bueskydning</h2>
-
-        <div class="expandable">
-            <div class="expandableHeader">
-                <h3>HVOR KAN JEG PRØVE BUESKYDNING?</h3>
-                <span class="toggleIcon">+</span>
-            </div>                
-            <div class="expandableContent">
-                <p>Kunne du godt tænke dig at prøve bueskydning? Så har du flere muligheder!</p>
-                <ul>
-                    <li>Du er altid velkommen til at tage fat i den lokale bueforening. Du kan finde din lokale forening her.</li>
-                    <li>Den første søndag i september afholder Bueskydning Danmark i samarbejde med interesserede foreninger ´Buernes Dag´. Buernes dag er et gratis åbent hus arrangement, hvor Bueskydning Danmark og din lokale bueklub inviterer alle interesserede til at komme og prøve kræfter med bueskydning.  Læs mere om ´Buernes Dag´ her.</li>
-                    <li>Bueskydning Danmark er også en del af DIF Skole OL, hvor 5. og 6. klasse kan dyste i skiveskydning. Læs mere om bueskydning i DIF Skole OL her. </li>
-                    <li>Bueskydning Danmark er også en del af DIF Soldaterprojekt. Det er et ønske om at bidrage til processen for at danske veteraner kan indgå i den almindelige foreningsidræt. Den proces kræver særlige hensyn, og projektet handler om at etablere platforme, hvor veteranerne i trygge rammer kan motiveres til at komme i gang med idræt. Du kan finde læse mere samt finde de foreninger, som er en del af DIF Soladterprojekt her.</li>
-                </ul>
-            </div>
-         </div>
-         <div class="expandable">
-             <div class="expandableHeader">
-                 <h3>ER BUESKYDNING SVÆRT SOM NYBEGYNDER?</h3>
-                 <span class="toggleIcon">+</span>
-             </div>             
-             <div class="expandableContent">
-                 <p>Det er let at lære de indledende trin i bueskydning, som at få fat i en bue og pil og forstå den grundlæggende mekanik, er ligetil. Det er svært at mestre at opnå ensartet præcision og udvikle en solid teknik tager tid og gentagelse.</p>
-                <p>Bueskydning kræver tålmodighed og en vilje til at øve sig regelmæssigt. Forvent ikke at blive ekspert med det samme. Tålmodighed er nøglen!</p>
-                <p>Bueskydningsforeninger under Bueskydning Danmark står klar til at lære, rådgive og vejlede dig til en god start i bueskydning!</p>
-            </div>
-        </div>
-        <div class="expandable">
-            <div class="expandableHeader">
-                <h3>ER DER EN ALDERSGRÆNSE FOR BUESKYDNING?</h3>
-                <span class="toggleIcon">+</span>
-            </div>
-            <div class="expandableContent">
-                <p>Bueskydning er for alle alder! Dog har de fleste bueforeninger en minimumsalder omkring de 10 år. Minimumsalderen er grundet sikkerhed og even til at trække buen. Det er op til foreningerne selv i forhold til at have en minimumsalder samt hvad den er. Derfor bør du høre din lokale forening, om de har en minimumsalder for at gå til bueskydning.</p>
-            </div>
-        </div>
-        <div class="expandable">
-            <div class="expandableHeader">
-                <h3>ER DER FORSKELLIGE DISCIPLINER I BUESKYDNING?</h3>
-                <span class="toggleIcon">+</span>
-            </div>
-        </div>
-
-        <section>
-            <video src="@/assets/Bueskydning-Danmark-Profilfilm.mp4"></video>
-            <h2>Hvad er bueskydning?</h2>
-            <p>Bueskydning er en sport og fritidsbeskæftigelse for alle. Drenge og piger i alle aldre kan være med og få sig en hyggelig stund på skydebanen eller i skoven.</p>
-            <p>Bueskydning er en individuel sport og man kan frit bestemme selv, hvor meget man vil gøre ud af bueskydningen. Mange har stor fornøjelse af at bruge bueskydningen som rekreation og afslapning – og til at koble fra med. Andre sætter sig mål som de forsøger at opnå, hvorefter de sætter nye mål og atter andre har evnerne til at blive blandt verdens bedste bueskytter.</p>
-            <p>Bueskydning kræver mest af alt koncentration og at man bliver i stand til at gøre de samme bevægelser skud for skud. Det er forholdsvist svært at opnå høj præcision og mange bliver bidt af jagten på at kunne skyde sine pile ens hver gang. Bueskydning skærper ens evne til at fordybe sig, skærper ens tålmodighed og skærper ens evner til at være rolig i stressede situationer. Alt sammen og mere til, er det egenskaber, der er gode at have med sig ud i dagligdagen.</p>
-            <p>I mange tilfælde kan man få adgang til sin klubs skydebaner på alle tidspunkter, hvorfor man har mulighed for at træne på de tidspunkter der passer én bedst, udover de fastsatte træningstidspunkter. Det sociale samvær over en kop kaffe eller sodavand er ofte højt værdsat blandt bueskytterne i alle aldre, hvilket gør træningssamlingerne til hyggelige stunder som man ser frem til.</p>
-        </section>
-
-   
-        <section>
-            <h2>Hvorfor vælge bueskydning?</h2>
-            <p>Der er mange gode grunde til at vælge at gå til bueskydning. Bueskydning kræver nemlig præcision, kontrol, fokus, fysisk og beslutsomhed. Uanset om du er topatlet eller bare dyrker bueskydning i din fritid, om du praktisere bueskydning indendørs eller udendørs, konkurrencemæssigt eller socialt – og på trods af at det ofte opfattes som stationært – gør bueskydning dig aktiv, forbrænder kalorier og i bedre humør. Bueskydning har flere fordele for dit helbred. Her kommer der 8 gode grunde til at vælge bueskydning:</p>
-            <dl>
-                <dt>Motion</dt>
-                <dd>Bueskydning er en sport, der kan dyrkes af alle, uanset alder, køn eller evner. Du bruger din fysisk, når du skal spænde buen samt bruger du også energi på at gå frem og tilbage mellem skydelinjen og skiverne for at hente pilene.</dd>
-                <dd>Under de Olympiske Lege i London i 2012 sammenlignede The Economist kalorieforbruget ved at vinde olympisk guld med 10 minutters andre aktiviteter som dans, snorkling, fejning og støvsugning. På vej til toppen af ​​det olympiske podie forbrænder en mandlig bueskytte anslået 1.084 kalorier – kun 747 kalorier mindre end kvinder i maraton, men mere end 1.054 end mændene på 100 meter sprint. (Ja, tidsrammerne er forskellige – men pointen er gyldig!)</dd>
-                <dd>Ved turneringer går bueskytter omkring otte kilometer hver dag og kan forbrænde mellem 100 og 150 aktive kalorier hvert 30. minut.</dd>
-
-                <dt>Styrke</dt>
-                <dd>Bueskydning kræver korte energiudbrud fra kernemusklerne, idet at spænde en bue skaber spændinger i bryst, hænder, arm og store øvre rygmuskler samt ikke-kerneområder af din anatomi, fx rotatormanchettene, der støtter dine skuldre. Den korrekte og kontinuerlige gentagelse af denne bevægelse styrker disse væv.</dd>
-
-                <dt>Kontrol</dt>
-                <dd>Bueskytter lærer at bevare fokus på deres skudrutine og undgå distraktioner som vind, afstand, støj og deres konkurrenter for at affyre den perfekte pil. Den kontrol er også gavnlig for andre områder af livet.</dd>
-
-                <dt>Koordination</dt>
-                <dd>De trin, der kræves for at skabe et godt skud, kombineres for at danne en præcisionsrutine, der skal indarbejdes i en bueskyttes muskelhukommelse og underbevidsthed. Der sker så meget på samme tid – og med så lille margen for fejl – at koordineringen af ​​alle disse bevægelser bliver næsten instinktiv.</dd>
-
-                <dt>Tålmodighed</dt>
-                <dd>Bueskydning er let at lære, men svært at perfektionere. Selvom det er sjovt, kan det være frustrerende – og tålmodighed i praksis er afgørende for en langsigtet fremtid i sporten. At have beslutsomheden til præcist at gentage en teknik gør dig til en bedre bueskytte og dermed til et bedre menneske!</dd>
-
-                <dt>Rolighed</dt>
-                <dd>At forblive rolig og præcis under pres er en evne, som bueskytter udvikler ved at styre deres vejrtrækning, koncentration og nervøsitet. Når du skyder godt, kan timerne gå på et øjeblik – og selvom sporten kan være social, handler bueskydning, når du er på banen, kun om dig og din bue. Det perfekte tidspunkt til lidt indre meditation.</dd>
-
-                <dt>Selvtillid</dt>
-                <dd>Selv i konkurrencer er en bueskyttes største modstander sig selv. Det er nemt at identificere, hvad man skal forbedre, og at sætte klare mål – resultater er trods alt baseret på let målbare scorer. Fremskridt avler selvtillid.</dd>
-
-                <dt>Social</dt>
-                <dd>Der er meget få sportsgrene i verden, der giver verdens bedste mulighed for at konkurrere om målet med en forholdsvis ny person. Turneringer i bueskydning er ofte uden barrierer – og muliggør venskaber mellem personer fra alle samfundslag. Man ved aldrig, hvem man ender med at skyde imod næste gang.</dd>
-            </dl>
-        </section>
         
         <section>
           <h2>Alt der er værd at vide om bueskydning</h2>
-<!-- Here goes the accordions, too much information -->
           <div v-for="(accordion, index) in accordions" :key="index" class="expandable">
         <div class="expandableHeader" @click="toggleAccordion(index)">
           <h3>{{ accordion.title }}</h3>
